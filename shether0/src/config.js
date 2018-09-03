07dc45f54c686107517c4124007c1be0d148f042
vlat <email2vlat@gmail.com>
2018-09-03 12:24:02 +0300

    initial commit

--- shether0/src/config.js
+++ shether0/src/config.js
@@ -1,5 +1,3 @@
 export const shconfig = {
-    mongo_con_url_old: 'mongodb://dpl:QazWsx321@ds020168.mlab.com:20168/ethgate',
-    mongo_con_url: 'mongodb://sketch:QazWsx321@ds143262.mlab.com:43262/sketchethe0',
     mongo_api_accounts_crud_url: 'https://api.mlab.com/api/1/databases/sketchethe0/collections/accounts?apiKey=ISpOrafbDytJQKPP-P0H3i4tToIYJrph',
 };
